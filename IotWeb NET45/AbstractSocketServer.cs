560dc4aa1f3e1cffbf1dc5e200ee386a58cce321
Shane Gough <shane@sensaura.org>
2015-12-16 13:33:59 +1000

    Starting to set up the project for a portable HTTP/WebSockets server.

---
+++ IotWeb NET45/AbstractSocketServer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using IotWeb.Common;
+using Splat;
+
+namespace IotWeb.Server
+{
+	public abstract class AbstractSocketServer : ISocketServer, IEnableLogger
+	{
+		// Instance variables
+		private bool m_running;
+		private ConnectionHandler m_handler;
+
+		public int Port { get; private set; }
+
+		public ConnectionHandler ConnectionRequested
+		{
+			get
+			{
+				return m_handler;
+			}
+
+			set
+			{
+				lock (this)
+				{
+					if (m_running)
+						throw new InvalidOperationException("Cannot change handler while server is running.");
+					m_handler = value;
+				}
+			}
+		}
+
+		public void Start(int port)
+		{
+			// Make sure we are not already running
+			lock (this)
+			{
+				if (m_running)
+					throw new InvalidOperationException("Socket server is already running.");
+				m_running = true;
+			}
+			Port = port;
+			// Set up the listener and bind
+			Socket listener = new Socket(SocketType.Stream, ProtocolType.IP);
+			listener.Bind(new IPEndPoint(IPAddress.Any, port));
+			listener.Blocking = true;
+			// Wait for incoming connections
+			while (m_running)
+			{
+				try
+				{
+					Socket client = listener.Accept();
+					if (m_handler != null)
+					{
+						string hostname = "0.0.0.0";
+						IPEndPoint endpoint = client.RemoteEndPoint as IPEndPoint;
+						if (endpoint != null) 
+							hostname = endpoint.Address.ToString();
+						ThreadPool.QueueUserWorkItem((arg) =>
+						{
+							try
+							{
+								NetworkStream input = new NetworkStream(client, FileAccess.Read, false);
+								NetworkStream output = new NetworkStream(client, FileAccess.Write, false);
+								m_handler(this, hostname, input, output);
+							}
+							catch (Exception ex)
+							{
+								this.Log().Debug("Connection handler failed unexpectedly - {0}", ex.Message);
+							}
+							// Finally, we can close the socket
+							client.Close();
+						});
+					}
+				}
+				catch (Exception)
+				{
+					// TODO: Handle this somehow?
+				}
+			}
+		}
+
+		public void Stop()
+		{
+
+		}
+
+	}
+}
---
+++ IotWeb Portable/ISocketServer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IotWeb.Common
+{
+	public delegate void ConnectionHandler(ISocketServer sender, string hostname, Stream input, Stream output);
+
+	public interface ISocketServer
+	{
+		ConnectionHandler ConnectionRequested { get; set; }
+
+		int Port { get; }
+
+		void Start(int port);
+
+		void Stop();
+	}
+}
