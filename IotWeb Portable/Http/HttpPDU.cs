6a8ca6d281b632f7c580169d98fbfaa10191bd7e
Shane Gough <shane@sensaura.org>
2015-12-17 10:44:36 +1000

    Fleshing out Http support classes.

---
+++ IotWeb Portable/Http/HttpPDU.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IotWeb.Common.Http
+{
+	/// <summary>
+	/// Base class for a HTTP protocol data unit (PDU).
+	/// 
+	/// This provides implementation details that are common to a request
+	/// and a response.
+	/// </summary>
+	public class HttpPDU
+	{
+		/// <summary>
+		/// The content type of the data associated with the operation
+		/// 
+		/// This property may be null (or the empty string) if no content is
+		/// available.
+		/// </summary>
+		public string ContentType { get; set; }
+
+		/// <summary>
+		/// The length (in bytes) of the content associated with the operation.
+		/// </summary>
+		public int ContentLength { get; set; }
+
+		/// <summary>
+		/// Only internal classes can extend this.
+		/// </summary>
+		internal HttpPDU()
+		{
+			ContentType = null;
+			ContentLength = 0;
+		}
+	}
+}
---
+++ IotWeb Portable/Http/HttpResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IotWeb.Common.Http
+{
+	/// <summary>
+	/// Represents a HTTP response
+	/// </summary>
+	public class HttpResponse : HttpPDU
+	{
+		internal HttpResponse()
+			: base()
+		{
+
+		}
+	}
+}
