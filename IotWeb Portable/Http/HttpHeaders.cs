0f07cef45c717498a92ebd4ad3ef15d37239667e
Shane Gough <shane@sensaura.org>
2015-12-18 20:02:01 +1000

    Split out CaseInsensitiveDictionary to a base class to be shared.

--- IotWeb Portable/Http/HttpHeaders.cs
+++ IotWeb Portable/Http/HttpHeaders.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace IotWeb.Common.Http
 {
-	public class HttpHeaders : IDictionary<string, string>
+	public class HttpHeaders : CaseInsensitiveDictionary<string>
 	{
 		// Common header fields
 		public const string Accept = "Accept";
@@ -31,106 +31,5 @@ namespace IotWeb.Common.Http
 		public const string SetCookie = "Set-Cookie";
 		public const string Server = "Server";
 		public const string WWWAuthenticate = "WWW-Authenticate";
-
-		// The actual headers
-		private IDictionary<string, string> m_headers = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
-
-		/// <summary>
-		/// Internal constructor
-		/// </summary>
-		internal HttpHeaders()
-			: base()
-		{
-			// Do nothing
-		}
-
-		#region Implementation of IDictionary
-		public bool IsReadOnly
-		{
-			get { return false; }
-		}
-
-		public ICollection<string> Keys
-		{
-			get { return m_headers.Keys; }
-		}
-
-		public ICollection<string> Values
-		{
-			get { return m_headers.Values; }
-		}
-
-		public int Count
-		{
-			get { return m_headers.Count; }
-		}
-
-		public string this[string key]
-		{
-			get
-			{
-				return m_headers[key];
-			}
-			set
-			{
-				m_headers[key] = value;
-			}
-		}
-
-		public void Add(string key, string value)
-		{
-			m_headers.Add(key, value);
-		}
-
-		public bool ContainsKey(string key)
-		{
-			return m_headers.ContainsKey(key);
-		}
-
-		public bool Remove(string key)
-		{
-			return m_headers.Remove(key);
-		}
-
-		public bool TryGetValue(string key, out string value)
-		{
-			return m_headers.TryGetValue(key, out value);
-		}
-
-		public void Add(KeyValuePair<string, string> item)
-		{
-			m_headers.Add(item);
-		}
-
-		public void Clear()
-		{
-			m_headers.Clear();
-		}
-
-		public bool Contains(KeyValuePair<string, string> item)
-		{
-			return m_headers.Contains(item);
-		}
-
-		public void CopyTo(KeyValuePair<string, string>[] array, int arrayIndex)
-		{
-			m_headers.CopyTo(array, arrayIndex);
-		}
-
-		public bool Remove(KeyValuePair<string, string> item)
-		{
-			return m_headers.Remove(item);
-		}
-
-		public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
-		{
-			return m_headers.GetEnumerator();
-		}
-
-		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-		{
-			return ((System.Collections.IEnumerable)m_headers).GetEnumerator();
-		}
-		#endregion
 	}
 }
