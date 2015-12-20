0606a199c156805a3cf4f96885e8187968356557
Shane Gough <shane@sensaura.org>
2015-12-20 15:43:45 +1000

    Added internal mime type database and utility methods to access the Assembly instance of a given class.

---
+++ IotWeb Portable/Http/MimeType.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using IotWeb.Common.Util;
+
+namespace IotWeb.Common.Http
+{
+    public static class MimeType
+    {
+        // Default type to use if not known
+        public const string DEFAULT = "application/x-binary";
+
+        // Mimetype database
+        private static Dictionary<string, string> m_mimeTypes = new Dictionary<string, string>();
+
+        static MimeType()
+        {
+            // Load the database from resources
+            Assembly assembly = Utilities.GetContainingAssembly(typeof(MimeType));
+            StreamReader source = new StreamReader(assembly.GetManifestResourceStream(assembly.GetName().Name + ".Resources.mimetypes.txt"));
+            char[] separators = new char[] { ',' };
+            while (!source.EndOfStream)
+            {
+                string line = source.ReadLine();
+                line = line.Trim();
+                if ((line.Length == 0) || line.StartsWith("#"))
+                    continue;
+                string[] parts = line.Split(separators);
+                if (parts.Length == 2)
+                    m_mimeTypes[parts[0].Trim().ToLower()] = parts[1].Trim();
+            }
+        }
+
+        /// <summary>
+        /// Determine the mime type based on the extension or filename
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public static string FromExtension(string filename)
+        {
+            int lastDot = filename.LastIndexOf('.');
+            if (lastDot > 0)
+                filename = filename.Substring(lastDot);
+            string mimetype;
+            if (!m_mimeTypes.TryGetValue(filename.ToLower(), out mimetype))
+                mimetype = DEFAULT;
+            return mimetype;
+        }
+    }
+}
---
+++ IotWeb Portable/Util/Utilities.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IotWeb.Common.Util
+{
+    public static class Utilities
+    {
+        /// <summary>
+        /// Get the Assembly instance that contains the given type.
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static Assembly GetContainingAssembly(Type t)
+        {
+            // Type.Assembly is not available in PCLs targets UWP so we have to
+            // get the assembly name from the fully qualified class name and then
+            // load the assembly directly
+            string name = t.AssemblyQualifiedName;
+            int index = name.IndexOf(", ");
+            if (index >= 0)
+                return Assembly.Load(new AssemblyName(name.Substring(index + 2)));
+            return null;
+        }
+
+        /// <summary>
+        /// Get the assembly instance that contains the given type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static Assembly GetContainingAssembly<T>()
+        {
+            return GetContainingAssembly(typeof(T));
+        }
+    }
+}
