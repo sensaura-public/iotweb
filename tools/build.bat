cd478e07a18962d4efe4b7381428f697db225dda
Shane Gough <shane@sensaura.org>
2015-12-27 15:14:58 +1000

    Working on build script.

--- tools/build.bat
+++ tools/build.bat
@@ -7,26 +7,35 @@ REM - or better. The following tools must be on the path - Python, MSbuild
 REM - and NuGet.
 REM -------------------------------------------------------------------------
 
+REM Get the solution directory
+set OLDDIR=%CD%
+set TOOLDIR=%~dp0
+cd %TOOLDIR%..
+
 REM Make sure 'msbuild' is available
 where msbuild.exe >nul 2>nul
 if %errorlevel%==1 (
   @echo msbuild.exe not found in path.
-  exit 1
+  goto done
   )
 
 REM Make sure 'python' is available
 where python.exe >nul 2>nul
 if %errorlevel%==1 (
   @echo python.exe not found in path.
-  exit 1
+  goto done
   )
 
 REM Make sure 'nuget' is available
-where nuget.exe >nul 2>nul
-if %errorlevel%==1 (
-  @echo nuget.exe not found in path.
-  exit 1
+if not exist %TOOLDIR%\nuget.exe (
+  @echo nuget.exe is not in the tools directory
+  goto done
   )
 
 REM Update versions
+python %TOOLDIR%\bumpver.py
 
+REM Finish up
+:done
+cd %OLDDIR%
+cmd /k
---
+++ tools/bumpver.py
@@ -0,0 +1,81 @@
+#!/bin/env python
+#----------------------------------------------------------------------------
+# 27-Dec-2015 ShaneG
+#
+# Bump versions for .NET solutions
+#----------------------------------------------------------------------------
+from os.path import walk, join
+from sys import argv
+import re
+
+# Regex to find version stamps
+VERSION_REGEX = re.compile("\"([0-9]+\.[0-9]+\.[0-9]+)\"")
+
+def bumpVersion(line, bumpMinor):
+  match = VERSION_REGEX.search(line)
+  if not match:
+    return line
+  # Extract the start, version and end of the line
+  first = match.start(1)
+  last = match.end(1)
+  prefix = line[:first]
+  version = line[first:last]
+  suffix = line[last:]
+  # Split apart the version
+  v = list([ int(x) for x in version.split(".") ])
+  if bumpMinor:
+    v[1] = v[1] + 1
+  else:
+    v[2] = v[2] + 1
+  return "%s%s%s" % (prefix, ".".join([ str(x) for x in v]), suffix)
+
+def AssemblyVersion(filename, bumpMinor):
+  lines = list()
+  for line in open(filename, "r"):
+    index = line.find("AssemblyVersion")
+    if index >= 0:
+      line = bumpVersion(line, bumpMinor)
+    index = line.find("AssemblyFileVersion")
+    if index >= 0:
+      line = bumpVersion(line, bumpMinor)
+    lines.append(line)
+  # Write the new version
+  output = open(filename, "w")
+  output.write("".join(lines))
+  output.close()
+
+def PackageVersion(filename, bumpMinor):
+  lines = list()
+  for line in open(filename, "r"):
+    index = line.find(" Version=")
+    if index >= 0:
+      line = bumpVersion(line, bumpMinor)
+    lines.append(line)
+  # Write the new version
+  output = open(filename, "w")
+  output.write("".join(lines))
+  output.close()
+
+# Map version file names to the handler function
+VERFILES = {
+  "AssemblyInfo.cs": AssemblyVersion,
+  "Package.appxmanifest": PackageVersion,
+  }
+
+def checkFile(arg, dirname, names):
+  for verfile in VERFILES.keys():
+    if verfile in names:
+      arg.append((dirname, verfile))
+
+if __name__ == "__main__":
+  # Figure out what to bump
+  bumpMinor = False
+  if (len(argv) == 2) and (argv[1] == "minor"):
+    bumpMinor = True
+  # Find the files we need to change
+  files = list()
+  walk(".", checkFile, files)
+  for path, name in files:
+    fullname = join(path, name)
+    VERFILES[name](fullname, bumpMinor)
+    print "Processed '%s'" % fullname
